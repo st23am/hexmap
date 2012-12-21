815a04885f3a5ee734c655e8c26aa6986bbe8378
James Smith <st23am@gmail.com>
2012-12-21 14:41:28 -0500

    add backbone

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,6 +4,10 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
 //
+//= require underscore
+//= require raphael
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
+//= require backbone
+//= require app
+//= require ./views/grid
